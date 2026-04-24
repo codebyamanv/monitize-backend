bf62c824c8cd0fc2a41887fb521f977b853eff20
Aman Verma <amanverma0428@gmail.com>
2026-04-24 15:38:42 +0530

    fix domain

--- src/utils/sessionUtils.js
+++ src/utils/sessionUtils.js
@@ -4,19 +4,11 @@ export const generateSessionToken = () => {
     return crypto.randomBytes(32).toString('hex')
 }
 
-// export const cookieOptions = {
-//     httpOnly: true,
-//     secure: true,
-//     sameSite: 'none',
-//     path: '/',
-//     // domain: 'Enter your domain here',
-//     maxAge: 7 * 24 * 60 * 60 * 1000,
-// }
 export const cookieOptions = {
     httpOnly: true,
     secure: true,
     sameSite: 'none',
     path: '/',
-    domain: ['monitize.vercel.app', 'monitize-admin.vercel.app'],
+    // domain: 'Enter your domain here',
     maxAge: 7 * 24 * 60 * 60 * 1000,
 }
