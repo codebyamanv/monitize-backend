fbcad3e1768dcfb2691aba2c83eb77fceb9162e3
Aman Verma <amanverma0428@gmail.com>
2026-04-24 15:27:10 +0530

    fix import

--- src/controllers/cities.controller.js
+++ src/controllers/cities.controller.js
@@ -1,8 +1,7 @@
 import Cities from '../models/cities.model.js'
 import asyncHandler from '../utils/asyncHandler.js'
-import ApiResponse from '../utils/ApiResponse.js'
+import ApiResponse from '../utils/apiResponse.js'
 export const getCities = asyncHandler(async (req, res) => {
-    console.log(req.query.country)
     const cities = await Cities.find({ country_name: req.query.country }).select('name').lean()
     return ApiResponse.success(cities).send(res)
 })
